fa358f6b7097cd1ad0cb1aaef5ecd67e0ee819a0
Nik985Gamer <115098287+Nik985Gamer@users.noreply.github.com>
2022-11-07 20:20:45 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,71 @@
-<?php
+<html lang="en">
 
-echo "Se venden TACOS de RATA BIEN RICOS en la esquina!";
-?>
+<head>
+  <meta charset="utf-8">
+
+  <title>Html Generated</title>
+  <meta name="description" content="Figma htmlGenerator">
+  <meta name="author" content="htmlGenerator">
+  <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="styles.css">
+
+  <style>
+    body {
+      background: #E5E5E5;
+    }
+  </style>
+
+</head>
+
+<body>
+  <div class=e1_6>
+    <div class=e1_41>
+      <div class=e1_15><span class="e1_8">En esta pagina web encontraras informacion acerca de mi canal de YouTube.
+          Primero que nada quiero agradecerte por tomarte el tiempo de entrar a este sitio, espero que te guste mucho y
+          puedas ayudarme a crecer. Ahora bien, mi canal esta enfocado a subir gameplays de diversos juegos, ya sean de
+          PC o para moviles. De igual manera subo juegos de cualquier genero, tanto de accion, shooters, de carreras,
+          supervivencia, aventuras y de mi genero favorito, el cual es de terror.</span>
+        <div class="e1_14"></div>
+      </div>
+      <div class=e5_168>
+        <div class="e1_28"></div><span class="e1_40">Otras entradas</span>
+      </div>
+      <div class=e1_37>
+        <div class="e1_35"></div><span class="e1_36">Breve vistazo a mi contenido</span><br><br><br><br>
+        <iframe width="650" height="338" src="https://www.youtube.com/embed/ECRbCk9WwX0" title="Trailer del canal"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen></iframe>
+      </div>
+      <div class=e1_11>
+        <div class="e1_10"></div><span class="e1_9">Bienvenido a mi pagina web
+        </span>
+      </div>
+    </div>
+    <div class=e1_13>
+      <div class=e1_7>
+        <div class="e1_4"></div>
+      </div>
+      <div class="e1_12"></div>
+    </div>
+    <div class="e5_171"></div>
+    <div class=e1_39>
+      <div class="e1_16"></div>
+      <div class=e1_21>
+        <div class="e1_22"><a href="contact us.html"></div><span class="e1_23">Contact us</span></a>
+      </div>
+      <div class=e1_20>
+        <div class="e1_17"><a href="about us.html"></div><span class="e1_19">About us</span></a>
+      </div>
+      <div class=e1_34>
+        <div class="e1_33"></div>
+        <div class="e1_32"></div>
+        <div class="e1_29"><a href="index.html"></div><span class="e1_30">Nik985's Channel</span></a>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html
